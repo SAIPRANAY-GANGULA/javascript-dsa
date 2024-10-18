81819f66615ee1b0d57a457fa7ce167b5ac543ad
SAIPRANAY-GANGULA <gangulasaiparanay@gmail.com>
2024-10-18 23:45:24 +0200

    slidingWindowMaximum

--- sliding-window-2-pointer/slidingWindowMaximum.js
+++ sliding-window-2-pointer/slidingWindowMaximum.js
@@ -1,16 +1,44 @@
 function slidingWindowMaximum(arr, k) {
   let n = arr.length;
   let ans = [];
-  let windowMax = 0;
+  let doubleEndedQueue = []; //storing indexes of elements which can be maximum
 
-  for (let i = 0; i < k; i++) {
-    windowMax = Math.max(windowMax, arr[i]);
+  //calculate maximum element for first window(first k elements)
+  doubleEndedQueue.push(0);
+  for (let i = 1; i < k; i++) {
+    //if element back of my deck is less than current element(i) then pop those elements which are back of the deck as they are useless
+    while (
+      doubleEndedQueue.length > 0 &&
+      arr[doubleEndedQueue[doubleEndedQueue.length - 1]] < arr[i]
+    ) {
+      doubleEndedQueue.pop();
+      console.log('de',doubleEndedQueue)
+    }
+    doubleEndedQueue.push(i);
+    console.log('de',doubleEndedQueue)
   }
-  ans.push(windowMax);
+  ans.push(arr[doubleEndedQueue[0]]);
 
-  for (let i = 1; i <= n - k; i++) {
-    windowMax = Math.max(windowMax, arr[i + k - 1]);
-    ans.push(windowMax);
+  // calaculating maximum for remaining window
+  for (let j = k; j < n; j++) {
+    const startingPoint = j - k + 1;
+    //if element is not part of current window then remove it from start
+    while (doubleEndedQueue.length > 0 && doubleEndedQueue[0] < startingPoint) {
+      doubleEndedQueue.shift();
+      console.log('de',doubleEndedQueue)
+    }
+
+    //if element back of my deck is less than current element(i) then pop those elements which are back of the deck as they are useless
+    while (
+      doubleEndedQueue.length > 0 &&
+      arr[doubleEndedQueue[doubleEndedQueue.length - 1]] < arr[j]
+    ) {
+      doubleEndedQueue.pop();
+      console.log('de',doubleEndedQueue)
+    }
+    doubleEndedQueue.push(j);
+    console.log('de',doubleEndedQueue)
+    ans.push(arr[doubleEndedQueue[0]]);
   }
 
   return ans;
